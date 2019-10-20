c9819a701042ac448e0ec26e7341b8959d4383c1
seethersan <carlos_jcez@hotmail.com>
2019-10-20 06:29:58 -0500

    [feature] adding external url to Education Material

--- erpnext/templates/generators/educational_material.py
+++ erpnext/templates/generators/educational_material.py
@@ -9,15 +9,23 @@ import json
 from frappe import _
 from erpnext.education.doctype.education_settings.education_settings import get_secure_url
 
+def get_domain(url):
+    return url.split("//")[-1].split("/")[0].split('?')[0]
+
 @frappe.whitelist()
 def open_url(cicle, material):
     secure_url = get_secure_url()
     if "Unidad " in material:
         material = material.replace("Unidad ", "material_book_unit_")
+        secure_url = secure_url + "pdf/"
     else:
         material = "material_" + material.lower()
+        secure_url = secure_url + "audio/"
     educational_material = frappe.get_doc("Educational Material", cicle)
     url = educational_material.get(material)
-    response = requests.get(url)
-    encrypted_url = json.loads(response.content)
-    return secure_url + encrypted_url.get("url")
+    if get_domain(url) == get_domain(secure_url):
+        response = requests.get(url)
+        encrypted_url = json.loads(response.content)
+        return secure_url + encrypted_url.get("url")
+    else:
+        return url
