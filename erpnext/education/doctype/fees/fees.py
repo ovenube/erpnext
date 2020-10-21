4be68f9c1f8977768a952a8b3fc35a186ed35aa7
seethersan <carlos_jcez@hotmail.com>
2020-10-21 12:15:08 -0500

    [fix] typo error fixed

--- erpnext/education/doctype/fees/fees.py
+++ erpnext/education/doctype/fees/fees.py
@@ -121,7 +121,7 @@ class Fees(AccountsController):
 				program_enrollment = frappe.get_doc("Program Enrollment", program_list[0].name)
 				row = program_enrollment.append('courses', {})
 				row.course = component.fees_category
-				row.course_name = frappe.get_value("Course", component.fees_category, course_name)
+				row.course_name = frappe.get_value("Course", component.fees_category, "course_name")
 				program_enrollment.save(ignore_permissions=True)
 
 def get_fee_list(doctype, txt, filters, limit_start, limit_page_length=20, order_by="modified"):
